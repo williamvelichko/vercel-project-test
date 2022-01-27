72d08746f0551b78fb4f6ea61c69a3c008c5cdae
williamvelichko <williamvelichko2003@gmail.com>
2022-01-27 14:14:47 -0800

    added a background

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,15 @@
 import React from "react";
+import "./../styles/HomePage.css";
 
 function HomePage() {
   return (
-    <div className="homePage">
-      Home page
-      <img src="" alt="" />
+    <div>
+      <div className="homePage">
+        <div className="description">
+          <h2>BBC YOUTH</h2>
+          <h4>Welcome To Bible Babtist Church Youth</h4>
+        </div>
+      </div>
     </div>
   );
 }
