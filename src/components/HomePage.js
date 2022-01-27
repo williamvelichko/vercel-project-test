f593b9477fee3556e40aa6eba0ba1b236cfb51d4
williamvelichko <williamvelichko2003@gmail.com>
2022-01-27 14:38:59 -0800

    continuing doing the homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -10,6 +10,15 @@ function HomePage() {
           <h4>Welcome To Bible Babtist Church Youth</h4>
         </div>
       </div>
+      <div className="upcomingEvents">
+        <h2>Up Coming Events</h2>
+        <li>
+          <h5>October 18 - "Leaders that inspire us" Presentation</h5>
+          <h5>November 22 - "Remember to give thanks" Youth Dinner</h5>
+          <h5>December 13 - "For Unto Us a Child Is Born" Christmas Evening</h5>
+        </li>
+        <h3>Donations Accepted By CashApp - $DavidBalatskiy</h3>
+      </div>
     </div>
   );
 }
