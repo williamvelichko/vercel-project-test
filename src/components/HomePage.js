61fb7cd35f40c6ce1ff126fb7bd05f95d15e2e90
williamvelichko <williamvelichko2003@gmail.com>
2022-01-27 15:08:59 -0800

    added more features

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -19,6 +19,18 @@ function HomePage() {
         </li>
         <h3>Donations Accepted By CashApp - $DavidBalatskiy</h3>
       </div>
+
+      <div className="galleryBlog">
+        <div className="galleryInfo">
+          <h2>Gallery Blog</h2>
+          <p>Click here to look at our youth trips and events!</p>
+          <button>Gallery Blog</button>
+        </div>
+        <img
+          src="https://media.istockphoto.com/photos/group-of-people-holding-hands-praying-worship-believe-picture-id847575018?b=1&k=20&m=847575018&s=170667a&w=0&h=X6Tz7mZiR0PK3b58dP8hZ23s1bB-o0VRtCo3898drJk="
+          alt=""
+        />
+      </div>
     </div>
   );
 }
